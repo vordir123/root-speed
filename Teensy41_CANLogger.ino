22b2f080c16a7703d428869d4257fc23f6dd410a
vordir123 <38102938+vordir123@users.noreply.github.com>
2025-06-11 09:24:53 +0200

    Add PowerShell script to reinstall Arduino environment

--- Teensy41_CANLogger.ino
+++ Teensy41_CANLogger.ino
@@ -2,38 +2,50 @@
 #include <FlexCAN_T4.h>
 #include <SD.h>
 #include <SPI.h>
+#include <Wire.h>
 
-#define CAN_BAUD 500000
-#define SD_CS 10
-#define STATUS_LED 13
-#define UART_BAUD 115200
+#define CAN_BAUD     500000
+#define SD_CS        10
+#define STATUS_LED   13
+#define UART_BAUD    115200
+#define LOG_ROTATE_MS 60000
+#define LOG_MAX_SIZE (1024UL*1024UL*5)
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
 File logFile;
 uint32_t logIndex = 0;
 unsigned long lastRotate = 0;
-const unsigned long rotateInterval = 60000; // 1 min
+
+/* Simple anomaly detection using running average */
+uint32_t idSum = 0; uint32_t idCount = 0;
+bool checkAnomaly(const CAN_message_t &msg){
+  idSum += msg.id; idCount++;
+  uint32_t avg = idSum / idCount;
+  return (msg.id > avg * 2); // trivial check
+}
 
 void rotateLog(){
-  if(logFile){
-    logFile.close();
-  }
+  if(logFile) logFile.close();
   char name[20];
   sprintf(name, "log_%lu.csv", logIndex++);
   logFile = SD.open(name, FILE_WRITE);
+  if(logFile){
+    logFile.println("time,id,len,data");
+  }
 }
 
 void setup(){
   pinMode(STATUS_LED, OUTPUT);
   Serial.begin(UART_BAUD);
-  while(!Serial && millis() < 3000); // Wait for Serial
+  Serial1.begin(UART_BAUD);
+  while(!Serial && millis() < 3000);
 
   Can0.begin();
   Can0.setBaudRate(CAN_BAUD);
 
   if(!SD.begin(SD_CS)){
     Serial.println("SD init failed");
-  } else {
+  }else{
     rotateLog();
   }
 }
@@ -46,22 +58,23 @@ void loop(){
       logFile.printf("%lu,%X,%d,", millis(), msg.id, msg.len);
       for(int i=0;i<msg.len;i++) logFile.printf("%02X", msg.buf[i]);
       logFile.println();
+      if(logFile.size() > LOG_MAX_SIZE) rotateLog();
     }
-    // forward to ESP32 via Serial1
-    Serial1.print("ID:");
-    Serial1.print(msg.id, HEX);
-    Serial1.print(" ");
-    for(int i=0;i<msg.len;i++){
-      Serial1.print(msg.buf[i], HEX); Serial1.print(' ');
+    // forward important messages
+    if(checkAnomaly(msg)){
+      Serial1.print("ID:");
+      Serial1.print(msg.id, HEX);
+      Serial1.print(" ");
+      for(int i=0;i<msg.len;i++){
+        Serial1.print(msg.buf[i], HEX); Serial1.print(' ');
+      }
+      Serial1.println();
     }
-    Serial1.println();
     digitalWrite(STATUS_LED, LOW);
   }
 
-  if(millis() - lastRotate > rotateInterval){
+  if(millis() - lastRotate > LOG_ROTATE_MS){
     lastRotate = millis();
     rotateLog();
   }
 }
-
-
